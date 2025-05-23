6679720c2a228d9788d3f3996b8d4e612a7a663d
Dmitriy Shin <dimashin9@gmail.com>
2025-05-23 11:37:45 +0900

    updates

--- src/components/ContactsSection.jsx
+++ src/components/ContactsSection.jsx
@@ -1,52 +1,70 @@
-import {
-	FaPhone,
-	FaWhatsapp,
-	FaTelegram,
-	FaInstagram,
-	FaYoutube,
-} from 'react-icons/fa'
+import { FaPhone } from "react-icons/fa"
 
 const ContactsSection = () => {
-	return (
-		<section className='bg-[#f0f0ff] text-white py-16 px-6'>
-			<div className='max-w-6xl mx-auto text-center'>
-				<h2 className='text-4xl font-bold text-[#0e2cc2] mb-6'>Контакты</h2>
-				<p className='text-lg text-[#4a4a7b] max-w-3xl mx-auto'>
-					Свяжитесь с нами удобным способом!
-				</p>
+  return (
+    <section className="bg-[#f0f0ff] text-white py-16 px-6">
+      <div className="max-w-6xl mx-auto text-center">
+        <h2 className="text-4xl font-bold text-[#0e2cc2] mb-6">Контакты</h2>
+        <p className="text-lg text-[#4a4a7b] max-w-3xl mx-auto">
+          Свяжитесь с нами удобным способом!
+        </p>
 
-				<div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-8 mt-10 mx-auto w-full'>
-					{/* Ким Евгений */}
-					<div className='p-6 bg-white rounded-lg shadow-md hover:shadow-lg transition duration-300 flex flex-col items-center text-center border border-[#0e2cc2]/10'>
-						<FaPhone className='text-[#a330f0] text-4xl mb-4' />
-						<h3 className='text-xl font-semibold text-[#0e2cc2]'>
-							Ким Евгений
-						</h3>
-						<a
-							href='tel:+821042252627'
-							className='text-[#4a4a7b] mt-2 hover:text-[#a330f0] transition duration-200'
-						>
-							+82 10-4225-2627
-						</a>
-					</div>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-8 mt-10 mx-auto w-full">
+          {/* Ким Евгений */}
+          <div className="p-6 bg-white rounded-lg shadow-md hover:shadow-lg transition duration-300 flex flex-col items-center text-center border border-[#0e2cc2]/10">
+            <FaPhone className="text-[#a330f0] text-4xl mb-4" />
+            <h3 className="text-xl font-semibold text-[#0e2cc2]">
+              Ким Евгений
+            </h3>
+            <a
+              href="tel:+821042252627"
+              className="text-[#4a4a7b] mt-2 hover:text-[#a330f0] transition duration-200"
+            >
+              +82 10-4225-2627
+            </a>
+          </div>
 
-					{/* Виталий */}
-					<div className='p-6 bg-white rounded-lg shadow-md hover:shadow-lg transition duration-300 flex flex-col items-center text-center border border-[#0e2cc2]/10'>
-						<FaPhone className='text-[#a330f0] text-4xl mb-4' />
-						<h3 className='text-xl font-semibold text-[#0e2cc2]'>
-							Югай Виталий
-						</h3>
-						<a
-							href='tel:+821093441782'
-							className='text-[#4a4a7b] mt-2 hover:text-[#a330f0] transition duration-200'
-						>
-							+82 10-9344-1782
-						</a>
-					</div>
-				</div>
+          {/* Виталий */}
+          <div className="p-6 bg-white rounded-lg shadow-md hover:shadow-lg transition duration-300 flex flex-col items-center text-center border border-[#0e2cc2]/10">
+            <FaPhone className="text-[#a330f0] text-4xl mb-4" />
+            <h3 className="text-xl font-semibold text-[#0e2cc2]">
+              Югай Виталий
+            </h3>
+            <a
+              href="tel:+821093441782"
+              className="text-[#4a4a7b] mt-2 hover:text-[#a330f0] transition duration-200"
+            >
+              +82 10-9344-1782
+            </a>
+          </div>
 
-				{/* Соцсети */}
-				{/* <div className='mt-10 flex justify-center gap-6'>
+          <div className="p-6 bg-white rounded-lg shadow-md hover:shadow-lg transition duration-300 flex flex-col items-center text-center border border-[#0e2cc2]/10">
+            <FaPhone className="text-[#a330f0] text-4xl mb-4" />
+            <h3 className="text-xl font-semibold text-[#0e2cc2]">
+              Ким Константин
+            </h3>
+            <a
+              href="tel:+821099802444"
+              className="text-[#4a4a7b] mt-2 hover:text-[#a330f0] transition duration-200"
+            >
+              +82 10-9980-2444
+            </a>
+          </div>
+
+          <div className="p-6 bg-white rounded-lg shadow-md hover:shadow-lg transition duration-300 flex flex-col items-center text-center border border-[#0e2cc2]/10">
+            <FaPhone className="text-[#a330f0] text-4xl mb-4" />
+            <h3 className="text-xl font-semibold text-[#0e2cc2]">Пак Сергей</h3>
+            <a
+              href="tel:+821024735718"
+              className="text-[#4a4a7b] mt-2 hover:text-[#a330f0] transition duration-200"
+            >
+              +82 10-2473-5718
+            </a>
+          </div>
+        </div>
+
+        {/* Соцсети */}
+        {/* <div className='mt-10 flex justify-center gap-6'>
 					<a
 						target='_blank'
 						href='https://wa.me/821012345678'
@@ -76,9 +94,9 @@ const ContactsSection = () => {
 						<FaYoutube />
 					</a>
 				</div> */}
-			</div>
-		</section>
-	)
+      </div>
+    </section>
+  )
 }
 
 export default ContactsSection
